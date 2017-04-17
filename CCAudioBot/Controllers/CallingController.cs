04bbbc59ae7e3d689608a87fe97a95f0c4886405
Steven McKeown <steven.j.mckeown@gmail.com>
2017-04-17 11:05:10 -0400

    Initial Commit

---
+++ CCAudioBot/Controllers/CallingController.cs
@@ -0,0 +1,37 @@
+﻿using CCAudioBot.IVR;
+using Microsoft.Bot.Builder.Calling;
+using Microsoft.Bot.Connector;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CCAudioBot.Controllers
+{
+    // Only enable authentication once you've configured your bot in the Bot Framework Portal and 
+    // have set the BotId, MicrosoftAppId and MicrosoftAppPassword in the Web.Config for your bot 
+    [BotAuthentication]
+    [RoutePrefix("api/calling")]
+    public class CallingController : ApiController
+    {
+        public CallingController()
+              : base()
+        {
+            CallingConversation.RegisterCallingBot(c => new ClearComCallingBot(c));
+        }
+
+        // Callback route for Skype calling events.
+        // Make sure to set the Microsoft.Bot.Builder.Calling.CallbackUrl in web.config
+        [Route("callback")]
+        public async Task<HttpResponseMessage> ProcessCallingEventAsync()
+        {
+            return await CallingConversation.SendAsync(Request, CallRequestType.CallingEvent);
+        }
+
+        // Route for incoming call events
+        [Route("call")]
+        public async Task<HttpResponseMessage> ProcessIncomingCallAsync()
+        {
+            return await CallingConversation.SendAsync(Request, CallRequestType.IncomingCall);
+        }
+    }
+}
