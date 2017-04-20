38342628830b8b661c8ba157ec1e275bdb3959da
Steven McKeown <steven.j.mckeown@gmail.com>
2017-04-20 13:16:19 -0400

    add leave temp support

--- CCAudioBot/Controllers/MessagesController.cs
+++ CCAudioBot/Controllers/MessagesController.cs
@@ -10,6 +10,7 @@ using Newtonsoft.Json;
 using Microsoft.Bot.Builder.Dialogs;
 using CCAudioBot.Dialogs;
 using Microsoft.Bot.Builder.FormFlow;
+using CCAudioBot.Models;
 
 namespace CCAudioBot
 {
@@ -24,7 +25,41 @@ namespace CCAudioBot
         {
             if (activity.Type == ActivityTypes.Message)
             {
-                await Conversation.SendAsync(activity, () => new ClearComLuisDialog());
+                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+                // calculate something for us to return
+                //int length = (activity.Text ?? string.Empty).Length;
+
+                JoinOrLeaveAChannel joinOrLeave = await ParseUserInput(activity.Text);
+                bool JoinLeave = false;
+                string ChannelToUse = string.Empty;
+                string MemberToUse = string.Empty;
+                if (joinOrLeave.intents.Count() > 0)
+                {
+                    switch (joinOrLeave.intents[0].intent)
+                    {
+                        case "JoinOrLeaveAChannel":
+                            foreach (Models.Entity entity in joinOrLeave.entities)
+                            {
+                                switch (entity.type)
+                                {
+                                    case "JoinLeave":
+                                        JoinLeave = !((entity.entity.ToLower() == "remove") || (entity.entity.ToLower() == "leave"));
+                                        break;
+                                    case "ChannelToUse":
+                                        ChannelToUse = entity.entity;
+                                        break;
+                                    case "MemberToUse":
+                                        MemberToUse = entity.entity;
+                                        break;
+                                }
+                            }
+                            break;
+                    }
+                }
+
+                // return our reply to the user
+                Activity reply = activity.CreateReply($"You want { MemberToUse } to { JoinLeave } channel { ChannelToUse } ");
+                await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
             {
@@ -32,6 +67,28 @@ namespace CCAudioBot
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        public async Task<JoinOrLeaveAChannel> ParseUserInput(string strInput)
+        {
+            string retString = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/2c854884-394b-424f-9cc6-f660d81915b8?subscription-key=0091c7f5c04542249870c45525fe1fce&staging=true&verbose=true&timezoneOffset=0.0&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<JoinOrLeaveAChannel>(jsonResponse);
+
+                    return data;
+                }
+            }
+
+            return null;
         }
 
         private Activity HandleSystemMessage(Activity message)
